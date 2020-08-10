8f09efce18194e62029130aba6ee11db8f8c134e
amr ragab <amrragab132015@gmail.com>
2020-08-09 20:58:40 -0700

    New Changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagination';
 
 
 
@@ -12,20 +9,13 @@ import { IPagination } from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'E-Commerce';
-  products: IProduct[];
 
-
-
-  constructor(private http: HttpClient)
+  constructor()
     {
 
     }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:44395/api/products?pageSize=50').subscribe((response: IPagination) => {
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
+
   }
 }
