c7b08c251e5d4bc7d1acc91949260086e1c38798
amr ragab <amrragab132015@gmail.com>
2020-08-14 20:35:09 -0700

    New Changes

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-
+import { RouterModule } from '@angular/router';
+import { ToastrModule } from 'ngx-toastr';
+import { TestErrorComponent } from './test-error/test-error.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SectionHeaderComponent } from './section-header/section-header.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, TestErrorComponent,
+     ServerErrorComponent, NotFoundComponent,
+      SectionHeaderComponent
+    ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    BreadcrumbModule,
+    ToastrModule.forRoot(
+      {positionClass: 'toast-bottom-right',
+       preventDuplicates: true
+      })
+
   ],
-  exports: [NavBarComponent]
+  exports: [NavBarComponent, SectionHeaderComponent]
 })
 export class CoreModule { }
